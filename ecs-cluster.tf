46218d04a20ff8b776ea04d65045c6b475b89f5f
Dzmitry Rudnouski <dzmitry_rudnouski@epam.com>
2020-03-17 13:30:29 +0300

    assign_public_ip

--- ecs-cluster.tf
+++ ecs-cluster.tf
@@ -47,7 +47,7 @@ resource "aws_ecs_service" "this" {
   network_configuration {
     subnets         = "${var.subnets}"
     security_groups = "${var.security_groups}"
-    assign_public_ip = true
+    assign_public_ip = "${var.assign_public_ip}"
   }
 
   load_balancer {
