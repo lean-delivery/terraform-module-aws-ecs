7a8c4189f118052b01a52d7295e365ce321d5612
ignatovich-artem <Artsiom_Ihnatovich@epam.com>
2022-04-04 18:14:35 +0300

    remove project and environment from default tags

--- main.tf
+++ main.tf
@@ -4,8 +4,6 @@ data "aws_partition" "current" {
 locals {
   default_tags = {
     Name        = "${var.project}-${var.environment}"
-    Project     = var.project
-    Environment = var.environment
   }
 }
 
